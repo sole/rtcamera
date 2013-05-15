d7b34fa60ac451e64cef3d49097653db0e1c4d40
Soledad Penades <sole@mozilla.com>
2013-05-14 18:15:54 -0700

    getting there with the shader

--- js/rtcamera.js
+++ js/rtcamera.js
@@ -11,6 +11,9 @@
 
 	var video;
 	var gl;
+	var shaderProgram;
+	var vertexPositionBuffer, uvBuffer, mvMatrix, pMatrix;
+	var texture;
 
 	if (navigator.getMedia) {
 
@@ -48,10 +51,12 @@
 			}
 			video.play();
 		}, function (error) {
+			// TODO handle
 			console.log(error);
 		});
 
 	} else {
+		// TODO handle
 		console.log('Native device media streaming (getUserMedia) not supported in this browser.');
 	}
 
@@ -68,12 +73,12 @@
 		gl = initWebGL(canvas);
 
 		if (gl) {
-			gl.clearColor(1.0, 0.0, 0.0, 1.0);                      // Set clear color to black, fully opaque
-			gl.enable(gl.DEPTH_TEST);                               // Enable depth testing
-			gl.depthFunc(gl.LEQUAL);                                // Near things obscure far things
-			gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);      // Clear the color as well as the depth buffer.
+			initWebGLBuffers();
+			initTexture();
+			initShaders();
+			render();
 		}
-
+		
 		document.body.appendChild(video);
 
 	}
@@ -84,6 +89,12 @@
 
 		try {
 			gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+			gl.viewportWidth = canvas.width;
+			gl.viewportHeight = canvas.height;
+			
+			gl.enable(gl.DEPTH_TEST);
+			gl.depthFunc(gl.LEQUAL);
+
 		} catch(e) {}
 
 		// If we don't have a GL context, give up now
@@ -93,6 +104,158 @@
 
 		return gl;
 
+	}
+
+	function initWebGLBuffers() {
+		vertexPositionBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+		var vertices = [
+			1.0,  1.0,  0.0,
+			-1.0,  1.0,  0.0,
+			1.0, -1.0,  0.0,
+			-1.0, -1.0,  0.0
+		];
+		
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+		vertexPositionBuffer.itemSize = 3;
+		vertexPositionBuffer.numItems = 4;
+
+		uvBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+
+		var uvs = [
+			1.0, 1.0,
+			0.0, 1.0,
+			1.0, 0.0,
+			0.0, 0.0
+		];
+
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvs), gl.STATIC_DRAW);
+		uvBuffer.itemSize = 2;
+		uvBuffer.numItems = 4;
+
+		mvMatrix = mat4.create();
+		pMatrix = mat4.create();
+	}
+
+	function initTexture() {
+		texture = gl.createTexture();
+		texture.image = video;
+	}
+
+	function getShader(glContext, id) {
+		var shaderScript = document.getElementById(id);
+		if (!shaderScript) {
+			return null;
+		}
+
+		var str = "";
+		var k = shaderScript.firstChild;
+		while (k) {
+			if (k.nodeType == 3) {
+				str += k.textContent;
+			}
+			k = k.nextSibling;
+		}
+
+		var shader;
+		if (shaderScript.type == "x-shader/x-fragment") {
+			shader = glContext.createShader(gl.FRAGMENT_SHADER);
+		} else if (shaderScript.type == "x-shader/x-vertex") {
+			shader = glContext.createShader(gl.VERTEX_SHADER);
+		} else {
+			return null;
+		}
+
+		glContext.shaderSource(shader, str);
+		glContext.compileShader(shader);
+
+		if (!glContext.getShaderParameter(shader, glContext.COMPILE_STATUS)) {
+			alert(glContext.getShaderInfoLog(shader));
+			return null;
+		}
+
+		return shader;
+	}
+
+	function initShaders() {
+
+		var fragmentShader = getShader(gl, 'fs');
+		var vertexShader = getShader(gl, 'vs');
+
+		shaderProgram = gl.createProgram();
+		gl.attachShader(shaderProgram, vertexShader);
+		gl.attachShader(shaderProgram, fragmentShader);
+		gl.linkProgram(shaderProgram);
+
+		if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+			alert('Could not initialise shaders');
+		}
+
+		gl.useProgram(shaderProgram);
+
+		shaderProgram.projectionMatrixUniform = gl.getUniformLocation(shaderProgram, 'projectionMatrix');
+		shaderProgram.modelViewMatrixUniform = gl.getUniformLocation(shaderProgram, 'modelViewMatrix');
+		shaderProgram.mapUniform = gl.getUniformLocation(shaderProgram, 'map');
+		shaderProgram.uvAttribute = gl.getAttribLocation(shaderProgram, 'uv');
+		shaderProgram.positionAttribute = gl.getAttribLocation(shaderProgram, 'position');
+		
+		gl.enableVertexAttribArray(shaderProgram.uvAttribute);
+		gl.enableVertexAttribArray(shaderProgram.positionAttribute);
+	}
+
+	function updateTexture(texture, video) {
+		
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+		//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+		//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		gl.bindTexture(gl.TEXTURE_2D, null);
+	}
+
+	function render() {
+
+		if( video.readyState === video.HAVE_ENOUGH_DATA ) {
+			updateTexture(texture, video);
+		}
+
+		gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+		gl.clearColor(1.0, 0.0, 0.0, 1.0);
+		
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+		mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+		//left, right, bottom, top, near, far
+		//mat4.ortho(pMatrix, -1, 1, -1, 1, 0.1, 1000);
+		var w = gl.viewportWidth,
+			h = gl.viewportHeight;
+		
+		//mat4.ortho(pMatrix, -w/2, w/2, -h/2, h/2, 0.1, 1000);
+        mat4.identity(mvMatrix);
+
+		//mat4.translate(mvMatrix, [0.0, 0.0, 0.0]);
+		mat4.translate(mvMatrix, [0.0, 0.0, -2.0]);
+
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.uniform1i(shaderProgram.mapUniform, 0);
+	
+		gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+		gl.vertexAttribPointer(shaderProgram.uvAttribute, uvBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+		gl.vertexAttribPointer(shaderProgram.positionAttribute, vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		
+		gl.uniformMatrix4fv(shaderProgram.projectionMatrixUniform, false, pMatrix);
+		gl.uniformMatrix4fv(shaderProgram.modelViewMatrixUniform, false, mvMatrix);
+
+        
+		gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexPositionBuffer.numItems);
 	}
 
 })();
